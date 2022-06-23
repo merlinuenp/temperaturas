f45bff69daff2715193df9fc810b0b1af7cedabc
jose <josermerlin@gmail.com>
2022-06-23 16:36:30 -0300

    v1.0.1

--- src/main/java/com/ConversorController.java
+++ src/main/java/com/ConversorController.java
@@ -12,27 +12,13 @@ public class ConversorController implements Serializable {
     private Double fahrenheit;
     
     public ConversorController(){
-        System.out.println("entrou");
         conversor = new Conversor(); 
         celsius = 0.0;
         fahrenheit = 0.0; 
     }
     
     public void converter(){
-        try{
         fahrenheit = conversor.converterParaFahrenheit(celsius);
-        }
-        catch(Exception e ){
-            System.out.println("...................erro");
-        }
-    }
-
-    public Conversor getConversor() {
-        return conversor;
-    }
-
-    public void setConversor(Conversor conversor) {
-        this.conversor = conversor;
     }
 
     public Double getCelsius() {
